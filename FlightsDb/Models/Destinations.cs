4ec817b32cee4ac7c8799aa9b090d856adc49bf0
Hadar Alliance <shiftan92@gmail.com>
2019-12-09 18:05:21 +0200

    המשך יצירת טבלת יעדים

---
+++ FlightsDb/Models/Destinations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightsDb.Models
+{
+    public class Destinations
+    {
+        public string City { get; set; }
+        public string Code { get; set; }
+        public double LenLat { get; set; }
+        public double LenLon { get; set; }
+
+    }
+}
