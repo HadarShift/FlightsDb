b018fd8d9b4c598afebd184aa3efe08e2ed33ee2
Hadar Alliance <shiftan92@gmail.com>
2019-12-09 17:15:04 +0200

    start

---
+++ FlightsDb/Controllers/FlightController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FlightsDb.Models;
+
+namespace FlightsDb.Controllers
+{
+    public class FlightController : ApiController
+    {
+        Flights flight = new Flights();
+
+        // GET api/values
+        public List<Flights> Get()
+        {
+            return flight.getFlightList();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        [Route ("api/flight/stop/{city}")]
+        public List<Flights> getFilteredRoutes(string city)
+        {
+            return flight.getFilteredConnection(city);//שולח עיר לסינון
+            
+        }
+
+        // POST api/values
+        public void Post([FromBody]Flights flights)
+        {
+            Flights.flightsList.Add(flights);
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FlightsDb/Models/Flights.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightsDb.Models
+{
+    public class Flights
+    {
+        public static List<Flights> flightsList = new List<Flights>();
+        public static List<Flights> fiteredConnectionList = new List<Flights>();
+        public string cityFrom { get; set; }
+        public string cityTo { get; set; }
+        public string dateFrom { get; set; }
+        public string dateUntil { get; set; }
+        public double Price { get; set; }
+        public List<string> Routes { get; set; }
+
+        internal List<Flights> getFlightList()
+        {
+            return flightsList;
+        }
+
+   
+
+        public Flights()
+        {
+        }
+
+        public List<Flights> getFilteredConnection(string City)
+        {
+            fiteredConnectionList = flightsList.Where(x => x.Routes.Contains(City)).ToList();
+            return fiteredConnectionList;
+        }
+      
+    }
+}
